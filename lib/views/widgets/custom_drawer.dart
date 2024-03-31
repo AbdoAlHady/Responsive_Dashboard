20a018c6f04b4bb8439de51775e1a76f674001ad
AbdoAlhady <abdo.agiry90@gmail.com>
2024-03-31 20:09:09 +0200

    finish drawer

--- lib/views/widgets/custom_drawer.dart
+++ lib/views/widgets/custom_drawer.dart
@@ -1,27 +1,49 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:responsive_dashboard/models/drawer_item_model.dart';
 import 'package:responsive_dashboard/utils/app_images.dart';
+import 'package:responsive_dashboard/views/widgets/active_and_inactive_item.dart';
 import 'package:responsive_dashboard/views/widgets/drawer_items_list_view.dart';
 import 'package:responsive_dashboard/views/widgets/user_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: const Column(
-        children: [
-          UserInfoListTile(
-            title: 'Lekan Okeowo',
-            subtitle: 'demo@gmail.com',
-            image: Assets.imagesAvatar3,
+      child: const CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: UserInfoListTile(
+              title: 'Lekan Okeowo',
+              subtitle: 'demo@gmail.com',
+              image: Assets.imagesAvatar3,
+            ),
           ),
-          SizedBox(height: 8),
-          DrawerItemsListView()
+          SliverToBoxAdapter(child: SizedBox(height: 8)),
+          DrawerItemsListView(),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                Expanded(child: SizedBox(height: 20,)),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                      title: "Setting system", image: Assets.imagesSettings),
+                ),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                      title: "Logout account", image: Assets.imagesLogout),
+                ),
+                SizedBox(height: 48),
+              ],
+            ),
+          )
         ],
       ),
     );
   }
 }
-
